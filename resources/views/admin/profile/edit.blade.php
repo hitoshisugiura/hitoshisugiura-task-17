ef76e323d4f85bdf9c6d1146572142f3b861ba93
hitoshisugiura <forseasonspicayahoo.co.jp@gmail.com>
2019-02-06 17:53:19 +0900

    first commit

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>プロフィール編集画面</h1>
+        <p>なんでもかんでもやるべき事で成長する。
+        </p>
+
+        <h1>頑張って見よう 諦めずやって見よう</h1>
+        <ui>
+        <li> 努力</li>
+        <li> 結果 </li>
+        <li> 成果</li>
+        <li> 昇給</li>
+        </ul>
+
+    </body>
+</html>
